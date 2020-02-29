680320e3cdead928ad939c92f0a44425c86c5881
nawaazwill11 <mastermindjim@gmail.com>
2020-02-29 21:03:20 +0530

    prep deploy

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = b'.e^\x11\xaeE.\x1cr\xfdX\x1c\x7f#Rc'
 
 
 class ProductionConfig(Config):
