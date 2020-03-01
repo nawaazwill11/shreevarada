964ba336408cd2ebb81c4b86e49bfc6a1c2fc0f4
nawaazwill11 <mastermindjim@gmail.com>
2020-03-01 08:25:37 +0530

    update static folder location

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
-import flask
-app = flask.Flask(__name__, static_url_path='/static')
+import flask, os
+
+static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
+
+app = flask.Flask(__name__, static_url_path='/static/', static_folder=static_folder)
 
 print('Web-app up and running!')
