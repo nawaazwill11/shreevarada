4680e41476c42abf068063ba0b1e393d2acbc1b6
nawaazwill11 <mastermindjim@gmail.com>
2020-03-02 17:01:32 +0530

    file layout restructured

--- app/static/src/js/components/parallax.js
+++ app/static/src/js/components/parallax.js
@@ -2,7 +2,7 @@ const parallax = function () {
     function init(option={}) {
         const elems = document.querySelectorAll('.parallax');
         const instances = M.Parallax.init(elems, option);
-
+        console.log(instances);
     }
 
     return {
