0d2fa75c9eb366f0893d6d137e7d477a3ded4953
nawaazwill11 <mastermindjim@gmail.com>
2020-03-15 12:14:54 +0530

    production css minify

--- app/minify-css.js
+++ app/minify-css.js
@@ -23,14 +23,3 @@ css_files.forEach(file => {
                         });
     }
 });
-
-// var input = fs.readFileSync('./static/dist/css/materialize.css', 'utf-8');
-// var options = { returnPromise: true };
-// var output = new cleanCSS(options)
-//                 .minify(input)
-//                 .then(minifile => {
-//                     fs.writeFileSync('./static/dist/css/materialize.min.css', minifile.styles);
-//                 })
-//                 .catch(error => {
-//                     console.log(`Shit got fucked like this => ` + error);
-//                 });
